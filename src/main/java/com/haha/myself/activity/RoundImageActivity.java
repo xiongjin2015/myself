1cce4d167e26623bb8574e6c63c5d77eb78a2cfd
xiongjin <xiongjin02@baidu.com>
2016-04-22 13:57:23 +0800

    custom round imageview

---
+++ src/main/java/com/haha/myself/activity/RoundImageActivity.java
@@ -0,0 +1,21 @@
+package com.haha.myself.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.haha.myself.R;
+
+public class RoundImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_round_image);
+
+    }
+
+}
