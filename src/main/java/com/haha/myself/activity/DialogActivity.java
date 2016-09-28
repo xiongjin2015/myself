daafe2859cc63eb1d799373aadf2ccdf278191c9
xj <xiongjin@ssports.com>
2016-09-28 18:18:20 +0800

    change to as 2.2 & srcoll hide layout impl

--- src/main/java/com/haha/myself/activity/DialogActivity.java
+++ src/main/java/com/haha/myself/activity/DialogActivity.java
@@ -2,8 +2,6 @@ package com.haha.myself.activity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
