e07db17873dc9d2305a3cdd13d7c0a09237dc50a
xj <xiongjin@ssports.com>
2018-08-21 16:01:31 +0800

    组件化demo

---
+++ app/src/test/java/com/haha/myself/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+package com.haha.myself;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+
+    private int writeTimeout = 10_000;
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+        System.out.print(writeTimeout);
+    }
+}
